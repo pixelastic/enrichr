be5bf9e6b8e4bcec13d1bb72b8fd5e8fb959572e
Pixelastic <tim@pixelastic.com>
2017-10-03 19:26:42 +0200

    0.0.10 - 2017-10-03
     * Adding geolocation through Algolia Places https://github.com/pixelastic/enrichr/commit/99971eccc5f3d55f92d26065791f4049ac01b08b

--- dist/src/index.js
+++ dist/src/index.js
@@ -16,9 +16,21 @@ var _requestPromise = require('request-promise');
 
 var _requestPromise2 = _interopRequireDefault(_requestPromise);
 
+var _algoliasearch = require('algoliasearch');
+
+var _algoliasearch2 = _interopRequireDefault(_algoliasearch);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _module = {
+  places: null,
+  initPlaces: function initPlaces() {
+    if (_module.places) {
+      return _module.places;
+    }
+    return _module.places = _algoliasearch2.default.initPlaces();
+  },
+
   // Given an url, will resolve an object of opengraph values
   opengraph: function opengraph(url) {
     return _bluebird2.default.promisify(_openGraph2.default)(url);
@@ -32,6 +44,11 @@ var _module = {
       resolveWithFullResponse: true
     }).then(function (data) {
       return data.request.href;
+    });
+  },
+  geolocation: function geolocation(address) {
+    return _module.initPlaces().search(address, { type: 'city' }).then(function (results) {
+      return results.hits[0]._geoloc;
     });
   }
 };
