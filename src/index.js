99971eccc5f3d55f92d26065791f4049ac01b08b
Pixelastic <tim@pixelastic.com>
2017-10-03 19:26:07 +0200

    Adding geolocation through Algolia Places

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import opengraph from 'open-graph';
 import Promise from 'bluebird';
 import request from 'request-promise';
+import algoliasearch from 'algoliasearch';
 
 const module = {
+  places: null,
+  initPlaces() {
+    if (module.places) {
+      return module.places;
+    }
+    return (module.places = algoliasearch.initPlaces());
+  },
   // Given an url, will resolve an object of opengraph values
   opengraph(url) {
     return Promise.promisify(opengraph)(url);
@@ -14,6 +22,12 @@ const module = {
       uri: url,
       resolveWithFullResponse: true,
     }).then(data => data.request.href);
+  },
+  geolocation(address) {
+    return module
+      .initPlaces()
+      .search(address, { type: 'city' })
+      .then(results => results.hits[0]._geoloc);
   },
 };
 export default module;
